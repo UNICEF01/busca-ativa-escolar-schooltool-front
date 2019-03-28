6784ddd889c5b85ddc86c46a29d3d37b19f3a548
Sandy Santos <sandysantos@mycrosan.com.br>
2019-03-27 23:56:16 -0300

    teste 2

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+export NVM_DIR="/home/krishna/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 /home/forge/.nvm/versions/node/v8.15.1/bin/npm install
 /home/forge/.nvm/versions/node/v8.15.1/bin/npm run build
