ba8c1117cf2c65540c49ea568427788e688143bd
Sandy Santos <sandysantos@mycrosan.com.br>
2019-03-27 23:48:42 -0300

    teste 9

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-
-git checkout testing
-git pull origin testing
 /home/forge/.nvm/versions/node/v8.15.1/bin/npm install
 /home/forge/.nvm/versions/node/v8.15.1/bin/npm run build
