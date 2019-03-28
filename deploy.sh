04609ba4a827658e7b7a30b8cf7a4c84dd06f801
Sandy Santos <sandysantos@mycrosan.com.br>
2019-03-27 23:29:52 -0300

    teste 5

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-if [ ! -d ~/.nvm ]; then
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
   source ~/.nvm/nvm.sh
   source ~/.profile
   source ~/.bashrc
   nvm install 8
-fi
+
 npm install
 npm run build
