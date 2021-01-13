4b0751e7383168b2cb116032c7c8e790ad9e842d
Sandy <ssantos@unicef.org>
2021-01-12 23:48:33 -0200

    Working

---
+++ src/views/Login.vue
@@ -0,0 +1,226 @@
+<template>
+  <v-container
+    fill-height
+    fluid
+    grid-list-xl>
+    <v-layout
+      justify-center
+      wrap>
+      <v-flex xs12 md8>
+        <material-card color="cyan" title="Login" text="">
+              <v-form ref="form_login" lazy-validation>
+                <v-container fluid>
+                  <v-flex xs12>
+                    <v-text-field label="Email" v-model="login.email" :rules="[rules.required, rules.email]"/>
+                  </v-flex>
+
+                  <v-flex xs12>
+                    <v-text-field
+                      class="purple-input"
+                      label="Senha"
+                      v-model="login.password"
+                      :append-icon="showPasword1 ? 'mdi-eye' : 'mdi-eye-off'"
+                      :rules="[rules.required, rules.min]"
+                      :type="showPasword1 ? 'text' : 'password'"
+                      counter
+                      @click:append="showPasword1 = !showPasword1"/>
+                  </v-flex>
+
+                </v-container>
+              </v-form>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+<!--                <v-btn color="default" v-if="!recoverPassword" text @click="recoverPassword = !recoverPassword">-->
+<!--                  Recuperar senha-->
+<!--                </v-btn>-->
+
+                <v-btn color="success" @click="loginUser()">
+                  Login
+                </v-btn>
+<!--                <v-btn v-if="recoverPassword" color="success" @click="resetPassword()">-->
+<!--                  Enviar-->
+<!--                </v-btn>-->
+              </v-card-actions>
+
+        </material-card>
+
+
+
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+
+</template>
+
+<script>
+  import Estados from "../components/core/CitySelect";
+  import {db, auth, usersCollection} from "./../firebase";
+
+  export default {
+    components: {Estados},
+    data() {
+      return {
+        login: {
+          email: '',
+          password: '',
+          confirmPassword: '',
+          error: '',
+        },
+        recoverPassword: false,
+        user: {
+          telefone: '',
+          uf: '',
+          city: '',
+          relation: {name: '', other: ''}
+        },
+        showPasword1: false,
+        showPasword2: false,
+        rules: {
+          required: value => !!value || 'Obrigatório.',
+          min: v => v.length >= 6 || 'Mínimo 6 caracteres',
+          passMatch: v => v === this.login.password || 'As senhas não correspondem',
+          email: v => /.+@.+\..+/.test(v) || 'E-mail inválido',
+        },
+        formHasErrors: false,
+
+        dialog: false,
+        descriptionLimit: 60,
+        items: [],
+        isLoading: false,
+        stateSelected: null,
+        citySelected: null,
+        search: null,
+        model: null,
+        count: null,
+        uf: '',
+        city: '',
+        value: '',
+        school: '',
+        quest: []
+      }
+    },
+    methods: {
+      checkAutenticate() {
+        if (auth.currentUser) {
+          this.$router.push({path: '/wash'})
+        }
+      },
+      // Triggered when `childToParent` event is emitted by the child.
+      onChildClick(value) {
+        this.user.uf = value.uf
+        this.user.city = value.city
+      },
+
+      async start() {
+        if (this.$refs.form_register.validate()) {
+          try {
+            let user = auth.createUserWithEmailAndPassword(this.login.email, this.login.password).then((user) => {
+              this.user.uid = user.user.uid
+              this.user.name = this.user.name.trim()
+              this.user.dt_create = new Date()
+              this.user.school = this.school
+
+              user.user.updateProfile({
+                displayName: this.user.name
+              })
+
+              db.collection("users").doc(user.user.uid).set(this.user)
+                .then(function () {
+                  // console.log()
+                })
+                .catch(function (error) {
+                  // console.error(error)
+                });
+              //console.log(user);
+              this.$router.push({path: '/wash'})
+              setInterval(function () {
+                window.location.reload();
+              }, 1000);
+            })
+          } catch (error) {
+            if (error.code === 'auth/email-already-in-use') {
+              this.$toast.open({
+                message: 'E-mail já cadastrado, clique em continuar de onde parou para fazer login',
+                type: 'error',
+                position: 'top'
+              });
+            }
+          }
+        }
+      },
+      loginUser() {
+        if (this.$refs.form_login.validate()) {
+          try {
+            const user = auth.signInWithEmailAndPassword(this.login.email, this.login.password).then((user) => {
+              //console.log(user);
+              this.$router.push({path: '/wash'})
+              setInterval(function () {
+                window.location.reload();
+              }, 1000);
+            })
+          } catch (error) {
+            console.log(error)
+          }
+        }
+      },
+      close() {
+        this.dialog = false;
+        this.recoverPassword = false;
+      },
+      resetPassword() {
+        auth.sendPasswordResetEmail(this.login.email).then((user) => {
+        });
+        this.$toast.open({
+          message: 'Um e-mail foi enviado para recuperar a senha!',
+          type: 'warning',
+          position: 'top'
+        });
+      }
+
+    },
+
+    watch: {
+      school(val) {
+        if (val != null)
+          this.school = val;
+      },
+      search(val) {
+        //Items have already been loaded
+        if (val.length < 3) return
+
+        this.isLoading = true
+
+        const requestOptions = {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify(
+            {
+              name: val,
+              id: this.user.city.id,
+              uf: this.user.uf,
+              city_id: this.user.city.ibge_city_id,
+              $hide_loading_feedback: true
+            }
+          )
+        }
+
+        // Lazily load input items ?XDEBUG_SESSION_START=PHPSTORM
+        fetch("https://api.buscaativaescolar.org.br/api/v1/open/schools", requestOptions)
+          .then(res => res.clone().json())
+          .then(res => {
+            this.items = res
+          })
+          .catch(err => {
+            console.log(err)
+          })
+          .finally(() => (this.isLoading = false))
+      },
+    },
+
+  }
+
+</script>
